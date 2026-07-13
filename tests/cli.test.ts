be6a18deece3efeb3324faf4b889ee3de034400d
Sebastian (Tiedtke) Huckleberry <sebastiantiedtke@gmail.com>
2026-07-13 09:32:41 -0700

    Fix runme binary download race (#24)
    Signed-off-by: Sebastian (Tiedtke) Huckleberry <sebastiantiedtke@gmail.com>

--- tests/cli.test.ts
+++ tests/cli.test.ts
@@ -1,6 +1,7 @@
 import cp from 'node:child_process'
 import { describe, it, expect, vi, afterAll } from 'vitest'
 
+import { download } from '../src/installer.js'
 import { runme } from '../src/cli.js'
 
 vi.mock('node:child_process', () => ({
@@ -10,21 +11,18 @@ vi.mock('node:child_process', () => ({
   }
 }))
 
-vi.mock('node:os', () => ({
-  default: {
-    type: vi.fn().mockReturnValue('Darwin'),
-    arch: () => 'arm64',
-    platform: vi.fn().mockReturnValue('darwin')
-  }
+vi.mock('../src/installer.js', () => ({
+  download: vi.fn().mockResolvedValue('/tmp/runme')
 }))
 
 describe('RunmeJS CLI', () => {
   it('supports defauls from config file', async () => {
     process.argv = ['nodepath', 'binPath', 'run', '--chdir=./examples', '--filename=example.md']
     await runme()
+    expect(download).toBeCalledTimes(1)
     expect(
       vi.mocked(cp.spawn).mock.calls[0][0]
-        .endsWith('.bin/runme run --chdir=./examples --filename=example.md')
+        .endsWith('/tmp/runme run --chdir=./examples --filename=example.md')
     ).toBe(true)
   })
 })
